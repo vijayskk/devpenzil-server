c860789cdac271cc36b5dd82d7dd603613ab56f7
ajoalex-neoito <ajo.a@neoito.com>
2022-01-09 16:03:04 +0530

    visitors

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const path = require("path");
 const visitors = require('./visitors.json')
 const app = express();
 
+app.get("/",(req,res)=>{
+  res.write("Welcome")
+  res.end()
+})
+
 app.get("/visitors", (req, res) => {
   res.status(200);
   let initdata = fs.readFileSync(path.resolve(__dirname, "visitors.json"));
@@ -15,7 +20,7 @@ app.get("/visitors", (req, res) => {
   res.end();
 });
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Port is running on ${PORT}`);
 });
